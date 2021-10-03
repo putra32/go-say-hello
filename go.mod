9eedeb5b6662b2cf06bb4fe0f36df7e3f2cf8bf7
putra32 <putralubis1432@gmail.com>
2021-10-03 19:26:13 +0700

    say hello v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/putra32/go-say-hello
+module github.com/putra32/go-say-hello/v2
 
 go 1.17
--- go_say_hello.go
+++ go_say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello Worl"
+func SayHello(name string) string {
+	return "Hello" + name
 }
